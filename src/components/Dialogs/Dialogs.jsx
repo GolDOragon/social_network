77aa3435fb2f5558ac713796629fac2ac6c5a5c6
Aleksey Palanevich <palanevich.aleksey@gmail.com>
2020-11-25 13:56:41 +0300

    feat: complete markup Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -32,6 +32,56 @@ const UserAvatar = (props) => {
   );
 };
 
+const CompanionAvatar = (props) => {
+  const { iconSrc, iconAltName, name, status } = props;
+
+  return (
+    <div className={s.correspondence__companion}>
+      <figure className={s.companion__avatar}>
+        <img className={s.companion__icon} src={iconSrc} alt={iconAltName} />
+      </figure>
+      <div className={s.companion__info}>
+        <p className={s.companion__name}>{name}</p>
+        <p className={s.companion__status}>{status}</p>
+      </div>
+    </div>
+  );
+};
+
+const Message = (props) => {
+  const { isMyMessage, iconSrc, iconAltName, status, text } = props;
+  let messageCSS = s.messages__message + ' ';
+  messageCSS += isMyMessage ? s.messages__message_me : s.messages__message_you;
+  let statusCSS = s.message__status + ' ';
+  switch (status) {
+    case 'online':
+      statusCSS += s.message__status_online;
+      break;
+    case 'away':
+      statusCSS += s.message__status_away;
+      break;
+    default:
+  }
+  let textCSS = s.message__text + ' ';
+  textCSS += isMyMessage ? s.message__text_me : s.message__text_you;
+
+  return (
+    <div className={messageCSS}>
+      <figure className={s.message__avatar}>
+        <img
+          className={s.message__icon}
+          src={iconSrc}
+          alt={iconAltName}
+          // src="http://www.wpkixx.com/html/winku/images/resources/friend-avatar8.jpg"
+          // alt="JasonBourne"
+        />
+        <span className={statusCSS}></span>
+      </figure>
+      <p className={textCSS}>{text}</p>
+    </div>
+  );
+};
+
 const Dialogs = () => {
   return (
     <div className={s.dialogs}>
@@ -88,29 +138,35 @@ const Dialogs = () => {
             .map((item) => item)}
         </ul>
         <div className={s.dialogs__correspondence}>
-          <div className={s.correspondence__companion}>
-            <figure className={s.companion__avatar}>
-              <img
-                className={s.companion__icon}
-                src="http://www.wpkixx.com/html/winku/images/resources/friend-avatar8.jpg"
-                alt="avatar"
-              />
-            </figure>
-            <div className={s.companion__info}>
-              <p className={s.companion__name}>Jason Bourne</p>
-              <p className={s.companion__status}>Online</p>
-            </div>
-          </div>
+          <CompanionAvatar
+            iconAltName="avatar"
+            iconSrc="http://www.wpkixx.com/html/winku/images/resources/friend-avatar8.jpg"
+            name="Jason Bourne"
+            status="online"
+          />
           <div className={s.correspondence__messages}>
-            <div>Hi</div>
-            <div
-              className={`${s.correspondence__message} ${s['correspondence__message']}`}
-            >
-              Hello, how are you?
-            </div>
+            <Message
+              isMyMessage={false}
+              iconSrc="http://www.wpkixx.com/html/winku/images/resources/friend-avatar8.jpg"
+              iconAltName="JasonBourne"
+              status="online"
+              text={`Hi, I'm interested in your vacancy. I haven' t commercial experience but I have done a lot of projects using JS / TS, Webpack, Babel, React / Redux, or Vue / Vuex (There's one team project on Vue).`}
+            />
+            <Message
+              isMyMessage={true}
+              iconSrc="http://www.wpkixx.com/html/winku/images/resources/friend-avatar4.jpg"
+              iconAltName="Good Boy"
+              status="offline"
+              text="I haven' t worked with some technologies of your stack, but I think I'll able to learn them in a short time."
+            />
+            <Message
+              isMyMessage={false}
+              iconSrc="http://www.wpkixx.com/html/winku/images/resources/friend-avatar8.jpg"
+              iconAltName="JasonBourne"
+              status="online"
+              text=" I haven' t worked with some technologies of your stack, but I I haven't worked with some technologies of your stack, but I I haven' t worked with some technologies of your stack, but I I haven't worked with some technologies of your stack, but I I haven' t worked with some technologies of your stack, but I I haven't worked with some technologies of your stack, but I think I' ll able to learn them in a short time. think I'll able to learn them in a short time. think I' ll able to learn them in a short time. think I'll able to learn them in a short time. think I' ll able to learn them in a short time. think I'll able to learn them in a short time. "
+            />
           </div>
-          <div className={s.messages__message}>Hi</div>
-          <div className={s.messages__message}>Hello</div>
         </div>
       </div>
     </div>
