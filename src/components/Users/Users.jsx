55da5bc019557d1f7a941b79fc5b50d1a540258c
Aleksey Palanevich <palanevich.aleksey@gmail.com>
2020-12-24 15:18:58 +0300

    refactor: divide Users on presentation and container component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,4 +1,3 @@
-import Axios from 'axios';
 import React from 'react';
 import Pagination from '../atoms/Pagination/Pagination';
 import SearchInput from '../atoms/SearchInput/SearchInput';
@@ -6,77 +5,57 @@ import Title from '../atoms/Title/Title';
 import UserItem from './UserItem/UserItem';
 import s from './Users.module.css';
 
-class Users extends React.Component {
-  componentDidMount() {
-    if (this.props.userList.length !== 0) return;
+const Users = (props) => {
+  const pageCount = Math.ceil(props.usersTotalCount / props.pageSize);
 
-    Axios.get(
-      `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
-    ).then((res) => {
-      this.props.setUsers(res.data.items);
-      this.props.setUsersTotalCount(res.data.totalCount);
-    });
-  }
-
-  handleChangePage = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber);
-    Axios.get(
-      `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
-    ).then((res) => {
-      this.props.setUsers(res.data.items);
-      this.props.setUsersTotalCount(res.data.totalCount);
-    });
+  const handleChangePage = (pageNumber) => {
+    props.onChangePage(pageNumber);
+  };
+  const handleSubscribe = (id) => {
+    props.onSubscribe(id);
+  };
+  const handleUnsubscribe = (id) => {
+    props.onUnsubscribe(id);
+  };
+  const handleChangeText = (text) => {
+    props.onChangeText(text);
   };
 
-  handleSubscribe = (id) => {
-    this.props.subscribe(id);
-  };
-
-  handleUnsubscribe = (id) => {
-    this.props.unsubscribe(id);
-  };
-
-  render = () => {
-    const pageCount = Math.ceil(
-      this.props.usersTotalCount / this.props.pageSize,
-    );
-
-    return (
-      <div className={s.users}>
-        <div className={s.users__title}>
-          <Title text={this.props.title} />
-        </div>
-        <div className={s.users__pagination}>
-          <Pagination
-            currentPage={this.props.currentPage}
-            pageTotalCount={pageCount}
-            onChangePage={this.handleChangePage}
+  return (
+    <div className={s.users}>
+      <div className={s.users__title}>
+        <Title text={props.title} />
+      </div>
+      <div className={s.users__pagination}>
+        <Pagination
+          currentPage={props.currentPage}
+          pageTotalCount={pageCount}
+          onChangePage={handleChangePage}
+        />
+      </div>
+      <main className={s.users__main}>
+        <div className={s.users__searchBox}>
+          <SearchInput
+            text={props.text}
+            onChangeInput={handleChangeText}
+            showSearchButton={false}
           />
         </div>
-        <main className={s.users__main}>
-          <div className={s.users__searchBox}>
-            <SearchInput
-              text={this.props.text}
-              onChangeInput={this.handleChangeSearchText}
-              showSearchButton={false}
-            />
-          </div>
-          <ul className={s.users__userList}>
-            {this.props.userList.map((user) => {
-              return (
-                <UserItem
-                  key={user.id}
-                  {...user}
-                  onSubscribe={this.handleSubscribe}
-                  onUnsubscribe={this.handleUnsubscribe}
-                />
-              );
-            })}
-          </ul>
-        </main>
-      </div>
-    );
-  };
-}
+        <ul className={s.users__userList}>
+          {props.userList.map((user) => {
+            return (
+              <UserItem
+                key={user.id}
+                {...user}
+                onSubscribe={handleSubscribe}
+                onUnsubscribe={handleUnsubscribe}
+              />
+            );
+          })}
+        </ul>
+      </main>
+    </div>
+  );
+};
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,3 +1,5 @@
+import Axios from 'axios';
+import React from 'react';
 import { connect } from 'react-redux';
 import {
   setCurrentPageAction,
@@ -42,6 +44,45 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    if (this.props.userList.length !== 0) return;
 
-export default UsersContainer;
+    Axios.get(
+      `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+    ).then((res) => {
+      this.props.setUsers(res.data.items);
+      this.props.setUsersTotalCount(res.data.totalCount);
+    });
+  }
+
+  handleChangePage = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    Axios.get(
+      `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
+    ).then((res) => {
+      this.props.setUsers(res.data.items);
+      this.props.setUsersTotalCount(res.data.totalCount);
+    });
+  };
+
+  handleChangeSearchText = (text) => {};
+  render = () => {
+    return (
+      <Users
+        usersTotalCount={this.props.usersTotalCount}
+        userList={this.props.userList}
+        pageSize={this.props.pageSize}
+        title={this.props.title}
+        currentPage={this.props.currentPage}
+        text={this.props.text}
+        onChangePage={this.handleChangePage}
+        onChangeText={this.props.onChangeText}
+        onSubscribe={this.props.subscribe}
+        onUnsubscribe={this.props.unsubscribe}
+      />
+    );
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
